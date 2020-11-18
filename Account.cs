8d65c7fc8a0a5eb2ec26ba43517fffd281184131
Ricardo Benitez <benitezc.ricardo@gmail.com>
2020-11-17 22:37:42 -0600

    [Code] Adds the DebitAccount.cs
    Signed-off-by: Ricardo Benitez <benitezc.ricardo@gmail.com>

--- Account.cs
+++ Account.cs
@@ -6,15 +6,33 @@ public enum AccountType
     Investment
 }
 
+public enum TransactionType
+{
+    Incoming,
+    Outgoing,
+}
+
 public class Account
 {
     protected string clientName;
     protected AccountType accountType;
+    protected decimal balance;
 
     public Account(string name, AccountType type)
     {
         clientName = name;
         accountType = type;
+    }
+    public decimal GetBalance() => balance;
+    public AccountType GetAccountType() => accountType;
+    public string GetClientName() => clientName;
+
+    public void SetTransaction(TransactionType transactionType, decimal amount)
+    {
+        if (transactionType == TransactionType.Incoming)
+            balance += amount;
+        else
+            balance -= amount;
     }
 }
 
---
+++ DebitAccount.cs
@@ -0,0 +1,8 @@
+
+
+public class DebitAccount : Account
+{
+    public DebitAccount(string name) : base(name, AccountType.Debit)
+    {
+    }
+}
